c1dbddac9c6b4d5a72465941e23f1703380c7e1e
luckynwj7 <44963879+luckynwj7@users.noreply.github.com>
2020-06-25 01:18:30 +0900

    [컴마나 쌍따옴표 read할 때 오류 수정]
    수정했습니다. 인코딩 문제가 남았습니다. 우선 reading이 안됩니다.

--- app/src/main/java/com/example/csvenglishworkbook/CsvReader.java
+++ app/src/main/java/com/example/csvenglishworkbook/CsvReader.java
@@ -2,7 +2,10 @@ package com.example.csvenglishworkbook;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 
 import java.util.ArrayList;
@@ -18,7 +21,6 @@ public class CsvReader {
             // open file.
             fr = new FileReader(file);
             bufrd = new BufferedReader(fr);
-
             // read 1 char from file.
             ch = (char) bufrd.read();
             while (ch != -1) {
@@ -85,6 +87,9 @@ public class CsvReader {
         String convertText = FileReadAndConvertToText(file);
         ArrayList<String> splitRowData = GetRowSplitTextList(convertText);
         for (String rowSplit:splitRowData) {
+            if(rowSplit.equals(",,")){
+                continue;
+            }
             ArrayList<String> splitColData = GetColumnSplitTextList(SaveTextErrorPosition(rowSplit)); // 에러 포지션을 저장하고, 그것에 대한것을 나눔
             resultList.add(splitColData);
         }
@@ -94,13 +99,17 @@ public class CsvReader {
     public static void SaveArrayListInDataBase(ArrayList<ArrayList<String>> inputArray, CustomSQLiteOpenHelper db){
         // db에 List를 저장해주는 함수
         // 넣을 때는 전부 string형태로 넣어줌. 내부에서 알아서 integer로 변환이 되어 DB에 저장됨
-        for (ArrayList<String> outterList:inputArray) {
-            System.out.println("집어 넣을 것들 : " + outterList.get(0) + ":" + outterList.get(1));
-            if(outterList.get(2).length()==0){
-                db.InsertData(outterList.get(0),outterList.get(1),"0");
+        for (ArrayList<String> outerList:inputArray) {
+            System.out.println("집어 넣을 것들 : " + outerList.get(0) + ":" + outerList.get(1));
+            if(outerList.get(0).length()==0 || outerList.get(1).length()==0){
+                //집어 넣을 가치가 없는 것들은 제외시킴
+                continue;
+            }
+            else if(outerList.get(2).length()==0){
+                db.InsertData(outerList.get(0),outerList.get(1),"0");
             }
             else{
-                db.InsertData(outterList.get(0),outterList.get(1),outterList.get(2));
+                db.InsertData(outerList.get(0),outerList.get(1),outerList.get(2));
             }
         }
     }
@@ -120,16 +129,13 @@ public class CsvReader {
         textErrorIndexList = new ArrayList<>();
         int innerPosition = 0;//쌍따옴표가 열렸을 때의 내부 위치를 반환
 
-        HashMap<Integer,String> errorPosition = null;
+        HashMap<Integer,String> errorPosition = new HashMap<Integer,String>();//에러 문자의 위치를 반환하는 리스트
 
         for(int charIndex=0;charIndex < inputTextLength; charIndex++) {
-
             if(innerDoubleQuotesFlag==false && inputText.charAt(charIndex)=='"') {
                 // 들어왔음을 의미함
                 innerDoubleQuotesFlag=true;
 
-                errorPosition = new HashMap<>(); //에러 문자의 위치를 반환하는 리스트
-                innerPosition = 0; // 문장위치 반환
 
                 inputText = inputText.substring(0,charIndex) + inputText.substring(charIndex+1); //현재 따옴표 삭제
                 inputTextLength--; // 삭제를 했으니 길이도 줄임
@@ -145,11 +151,15 @@ public class CsvReader {
                 inputTextLength--; // 삭제를 했으니 길이도 줄임
                 charIndex--; // 삭제했으니 반복수도 줄임
 
-                // 각각 저장해두고 다음으로
-                textErrorIndexList.add(errorPosition);
-
                 continue;
             }
+            else if(innerDoubleQuotesFlag==false && inputText.charAt(charIndex)==',') {
+                innerPosition = 0; // 문장위치 반환
+                textErrorIndexList.add(errorPosition);
+                errorPosition = new HashMap<Integer,String>();
+            }
+
+
 
             // 들어왔을 때 동작하는 함수
             if(innerDoubleQuotesFlag) {
@@ -169,13 +179,14 @@ public class CsvReader {
                 }
                 innerPosition++;
             }
+
         }
+        textErrorIndexList.add(errorPosition); //마지막 작업
         inputText = inputText.substring(0,inputTextLength); // 삭제할 거 전부 삭제하고 원래 배열로 돌아옴
         return inputText;
     }
 
     private static ArrayList<String> SplitTextHidingWordRevert(String[] splitArray) {
-        // 숨겨놓았던 문자(컴마나 따옴표)를 다시 삽입해서 ArrayList로 변환하여 돌려주는 함수
         if(textErrorIndexList==null) {
             System.out.println("사전에 정의된 배열이 없어 실행할 수 없음");
             return null;
@@ -183,16 +194,11 @@ public class CsvReader {
         int sentCount = 0;
         ArrayList<String> resultList = new ArrayList<String>();
         for (String text:splitArray) {
-            if(textErrorIndexList.size()<=sentCount){
-                resultList.add(text);
-                continue;
-            }
             for(int key:textErrorIndexList.get(sentCount).keySet()) {
-                System.out.println("ㅇㅇ"+text);
                 text = text.substring(0,key) + textErrorIndexList.get(sentCount).get(key) + text.substring(key);
             }
-            resultList.add(text);
             sentCount++;
+            resultList.add(text);
         }
         return resultList;
     }
@@ -200,8 +206,8 @@ public class CsvReader {
 
     public static void ShowDoubleArrayList(ArrayList<ArrayList<String>> inputArray){
         //디버깅전용 함수
-        for (ArrayList<String> outterList:inputArray) {
-            for (String innerStr:outterList) {
+        for (ArrayList<String> outerList:inputArray) {
+            for (String innerStr:outerList) {
                 System.out.print(innerStr + "|");
             }
             System.out.println("");
